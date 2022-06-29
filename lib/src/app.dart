fe26e8e0827a04e38cd6de00b00c4a19e0fcc799
uo272695 <uo272695@uniovi.es>
2022-06-29 20:41:31 +0200

    Clases de servicios creadas y clase de datos

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:restaurantes_gijon/src/dto/restaurant.dart';
+import 'package:restaurantes_gijon/src/view/restaurant_detail_view.dart';
+import 'package:restaurantes_gijon/src/view/restaurant_list_view.dart';
+
+import 'controller/settings_controller.dart';
+import 'view/settings_view.dart';
