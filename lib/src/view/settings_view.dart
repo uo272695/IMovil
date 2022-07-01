5c6a19fed1408cb981a9550ee424de23d0dd82c6
uo272695 <uo272695@uniovi.es>
2022-07-01 19:41:10 +0200

    Solucionando error dependencia flutter_gen (solucionado)

--- lib/src/view/settings_view.dart
+++ lib/src/view/settings_view.dart
@@ -1,7 +1,7 @@
 
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../controller/settings_controller.dart';
 
 class SettingsView extends StatelessWidget {
